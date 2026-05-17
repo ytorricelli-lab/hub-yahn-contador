9b916bfb05f27916de4adfad9efdd28e7e8ec51c
ytorricelli-lab <y.torricelli@gmail.com>
2026-05-17 17:32:17 -0300

    Home V4 premium

--- script.js
+++ script.js
@@ -7,25 +7,6 @@ const revealObserver = new IntersectionObserver((entries) => {
       revealObserver.unobserve(entry.target);
     }
   });
-}, { threshold: 0.16 });
+}, { threshold: 0.14 });
 
 revealEls.forEach((el) => revealObserver.observe(el));
-
-const tiltCards = document.querySelectorAll('.tilt-card');
-
-tiltCards.forEach((card) => {
-  card.addEventListener('mousemove', (event) => {
-    const rect = card.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-    const midX = rect.width / 2;
-    const midY = rect.height / 2;
-    const rotateY = ((x - midX) / midX) * 7;
-    const rotateX = -((y - midY) / midY) * 7;
-    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-  });
-
-  card.addEventListener('mouseleave', () => {
-    card.style.transform = '';
-  });
-});
